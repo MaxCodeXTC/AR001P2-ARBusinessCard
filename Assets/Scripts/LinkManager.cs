7e8d9eaefa6c0082f93ecbe5e9727862135bf119
Priyanshu Bhattacharjee <pbo4oh@gmail.com>
2022-03-22 12:11:21 +0530

    initial commit

---
+++ Assets/Scripts/LinkManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkManager : MonoBehaviour
+{
+    public void openLinkedin()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/priyanshu-bhattacharjee/");
+    }
+
+    public void openInstagram()
+    {
+        Application.OpenURL("https://www.instagram.com/priyanshucoderx/");
+    }
+
+    public void openGithub()
+    {
+        Application.OpenURL("https://github.com/Priyanshu-CODERX");
+    }
+
+    public void openDiscord()
+    {
+        Application.OpenURL("https://discord.gg/5bvZAZd83D");
+    }
+
+    public void openYoutube()
+    {
+        Application.OpenURL("https://youtube.com/c/coderslane");
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# AR001 Project 3: AR Car Customizer
+This is an AR Car Customzier app I developed for teaching Augmented Reality Development in AR001 Course.
